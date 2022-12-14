91e743a3fc171ef2a01c3ba84437fb964c28142d
Vitalii <hladon@meta,ua>
2022-12-14 21:48:17 +0200

    First review.

--- src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
+++ src/main/java/mate/jdbc/dao/ManufacturerDaoImpl.java
@@ -13,6 +13,13 @@ import mate.jdbc.util.ConnectionUtil;
 
 @Dao
 public class ManufacturerDaoImpl implements ManufacturerDao {
+
+    //TODO First you have to call executeUpdate() and only then getGeneratedKey().
+    /*TODO It`s bad way to handle exceptions. It going to crush whole app and don`t give any information about a reason.
+       You have to handle exception inside your catch or throw it further. Add more information about reason of exception.
+       In this task was suggested to transform exception DataProcessingException you should do this.
+     */
+    //TODO Move query to separate variable.
     @Override
     public Manufacturer create(Manufacturer manufacturer) {
         try (Connection connection = ConnectionUtil.getConnection();
@@ -33,7 +40,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
         }
         return manufacturer;
     }
-
+    //TODO Have to be returned Optional. If error occur or there is no record with such Id you can return Optional with Null.
     @Override
     public Manufacturer get(Long id) {
         Manufacturer manufacturer = new Manufacturer();
@@ -55,7 +62,9 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
         }
         return manufacturer;
     }
-
+    /*TODO Throwing RuntimeException is a bad idea because it unchecked exceptions.
+       And do not transform exception to its parent without a need. It`s shrink information about exceptions.
+     */
     @Override
     public List<Manufacturer> getAll() throws RuntimeException {
         List<Manufacturer> manufacturerList = new ArrayList<>();
@@ -77,7 +86,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
         }
         return manufacturerList;
     }
-
+    //TODO Statement.RETURN_GENERATED_KEYS is redundant here.
     @Override
     public void delete(Long id) {
         String deleteRequest = "UPDATE manufacturers SET is_deleted = 1 where id = ?";
@@ -91,7 +100,7 @@ public class ManufacturerDaoImpl implements ManufacturerDao {
             throw new RuntimeException("Error");
         }
     }
-
+    //TODO It will always return empty Manufacturer better return Manufacturer from input. And check is update was performed.
     @Override
     public Manufacturer update(Manufacturer manufacturer) {
         String updateRequest = "UPDATE manufacturers SET name = ?, country = ? WHERE id = ?";
